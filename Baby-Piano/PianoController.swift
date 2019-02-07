645bfc8f25494c09a609172c89e1df424c6e1692
Ankit Saxena <ankitatdelhii@ankits-macbook-air.local>
2019-02-08 01:32:35 +0530

    Working with different sounds!

--- Baby-Piano/PianoController.swift
+++ Baby-Piano/PianoController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // All buttons linked to single Action Outlet
     @IBAction func pianoButtonPressed(_ sender: UIButton) {
         print(sender.tag)
+        playSound(with: sender.tag)
+    }
+    
+    
+    func playSound(with noteName : Int){
+        if let soundURL = Bundle.main.url(forResource: "note\(noteName)", withExtension: ".wav"){
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            }
+            catch{
+                print(error)
+            }
+            audioPlayer.play()
+        }
+        
     }
     
 }
