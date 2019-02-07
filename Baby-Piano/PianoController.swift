0e8c8df03a280139dd58eaa0a1a00d6df0baa5dd
Ankit Saxena <ankitatdelhii@ankits-macbook-air.local>
2019-02-08 00:59:35 +0530

    Linked all buttons to same outlet

--- Baby-Piano/PianoController.swift
+++ Baby-Piano/PianoController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func pianoButtonPressed(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
 }
 
